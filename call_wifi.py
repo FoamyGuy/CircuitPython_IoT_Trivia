7839b1982d8b6c7b430673e0b5169b7308c61215
Kevin Thomas <kevin@mytechnotalent.com>
2020-11-18 17:32:57 -0500

    Update call_wifi.py

--- call_wifi.py
+++ call_wifi.py
@@ -15,11 +15,14 @@ def call_wifi():
     -------
     None
     """
-    # Setup wifi and connection
-    print(wifi.radio.connect(secrets['ssid'], secrets['password']))
-    print('ip', wifi.radio.ipv4_address)
-    display_text("ip: {}".format(wifi.radio.ipv4_address))
-    ipv4 = ipaddress.ip_address('8.8.8.8')
-    ping_result = wifi.radio.ping(ipv4)
-    print('ping', ping_result)
-    display_text("ping: {}".format(ping_result))
+    try:
+        # Setup wifi and connection
+        print(wifi.radio.connect(secrets['ssid'], secrets['password']))
+        print('ip', wifi.radio.ipv4_address)
+        display_text("ip: {}".format(wifi.radio.ipv4_address))
+        ipv4 = ipaddress.ip_address('8.8.8.8')
+        ping_result = wifi.radio.ping(ipv4)
+        print('ping', ping_result)
+        display_text("ping: {}".format(ping_result))
+    except:
+        return False
